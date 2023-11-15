a88679509cfda23e328b9c79fb477d64fed48e07
Julia Hehl <julia.hehl@bmw.de>
2023-11-15 15:56:46 +0100

    Added imports to preprocess data

---
+++ src/preprocessing/preprocess_data.py
@@ -0,0 +1,11 @@
+from data_cleaning import (
+    convert_string_to_geojson,
+    convert_polyline_to_geojson_format,
+    filter_invalid_trips,
+
+)
+
+from utils import (
+    convert_datatypes,
+
+)

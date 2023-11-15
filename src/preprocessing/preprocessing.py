95190a7c5d30bbd2dede36c80586d544a0b44a55
Julia Hehl <julia.hehl@bmw.de>
2023-11-15 10:35:08 +0100

    Formatting fixes

--- src/preprocessing/preprocessing.py
+++ src/preprocessing/preprocessing.py
@@ -1,24 +1,33 @@
-import pandas as pd
-import numpy as np
-from sklearn.metrics.pairwise import haversine_distances
 from math import radians
+
+import numpy as np
+import pandas as pd
+from sklearn.metrics.pairwise import haversine_distances
 
 
 def split_lat_lon(data):
     modified_data = data.copy()
-    modified_data['LON_SEQUENCE'] = modified_data.SEQUENCE.apply(lambda sequence_:
-                                                                 np.array([value_[1] for value_ in enumerate(sequence_)
-                                                                           if value_[0] % 2 == 0]))
-    modified_data['LAT_SEQUENCE'] = modified_data.SEQUENCE.apply(lambda sequence_:
-                                                                 np.array([value_[1] for value_ in enumerate(sequence_)
-                                                                           if value_[0] % 2 != 0]))
+    modified_data["LON_SEQUENCE"] = modified_data.SEQUENCE.apply(
+        lambda sequence_: np.array(
+            [value_[1] for value_ in enumerate(sequence_) if value_[0] % 2 == 0]
+        )
+    )
+    modified_data["LAT_SEQUENCE"] = modified_data.SEQUENCE.apply(
+        lambda sequence_: np.array(
+            [value_[1] for value_ in enumerate(sequence_) if value_[0] % 2 != 0]
+        )
+    )
     return modified_data
 
 
 def create_fix_length_sequences(data, n_limited) -> pd.DataFrame:
     modified_data = data.copy()
-    modified_data['START_SEQUENCE'] = modified_data.SEQUENCE.apply(lambda sequence: sequence[0:2 * n_limited])
-    modified_data['STOP_SEQUENCE'] = modified_data.SEQUENCE.apply(lambda sequence: sequence[-2 * n_limited:])
+    modified_data["START_SEQUENCE"] = modified_data.SEQUENCE.apply(
+        lambda sequence: sequence[0 : 2 * n_limited]
+    )
+    modified_data["STOP_SEQUENCE"] = modified_data.SEQUENCE.apply(
+        lambda sequence: sequence[-2 * n_limited :]
+    )
     return modified_data
 
 
@@ -28,28 +37,36 @@ def calculate_polyline_features(data) -> pd.DataFrame:
     calculates total flight time in seconds and minutes as TOTAL_FLIGHT_TIME_SECONDS and TOTAL_FLIGHT_TIME_MINUTES
     """
     modified_data = data.copy()
-    modified_data['N_COORDINATE_POINTS'] = modified_data['POLYLINE'].apply(lambda value: len(value))
+    modified_data["N_COORDINATE_POINTS"] = modified_data["POLYLINE"].apply(
+        lambda value: len(value)
+    )
     # total flight time
-    modified_data['TOTAL_FLIGHT_TIME_SECONDS'] = modified_data.apply(lambda row: (row.N_COORDINATE_POINTS - 1) * 15,
-                                                                     axis=1)
-    modified_data['TOTAL_FLIGHT_TIME_MINUTES'] = modified_data.TOTAL_FLIGHT_TIME_SECONDS / 60
+    modified_data["TOTAL_FLIGHT_TIME_SECONDS"] = modified_data.apply(
+        lambda row: (row.N_COORDINATE_POINTS - 1) * 15, axis=1
+    )
+    modified_data["TOTAL_FLIGHT_TIME_MINUTES"] = (
+        modified_data.TOTAL_FLIGHT_TIME_SECONDS / 60
+    )
     return modified_data
 
 
 def filter_invalid_trips(data, n_points: int) -> pd.DataFrame:
     """
-    filters trips with less than 10 coordinate points and takes data sample with longest 
+    filters trips with less than 10 coordinate points and takes data sample with longest
     POLYLINE for duplicated TRIP IDs
     """
     modified_data = data.copy()
     modified_data = modified_data[modified_data.N_COORDINATE_POINTS >= n_points]
     vc = modified_data.TRIP_ID.value_counts().reset_index()
-    duplicated_ids = vc[vc['count'] > 1].TRIP_ID.unique()
+    duplicated_ids = vc[vc["count"] > 1].TRIP_ID.unique()
     if len(duplicated_ids) > 0:
         duplicated_data = modified_data[modified_data.TRIP_ID.isin(duplicated_ids)]
         valid_data = modified_data[~modified_data.TRIP_ID.isin(duplicated_ids)]
-        filtered_data = duplicated_data.groupby('TRIP_ID').apply(
-            lambda data_chunk: data_chunk[data_chunk.N_COORDINATE_POINTS == data_chunk.N_COORDINATE_POINTS.max()])
+        filtered_data = duplicated_data.groupby("TRIP_ID").apply(
+            lambda data_chunk: data_chunk[
+                data_chunk.N_COORDINATE_POINTS == data_chunk.N_COORDINATE_POINTS.max()
+            ]
+        )
         modified_data = pd.concat([filtered_data, valid_data], axis=0)
     return modified_data
 
@@ -76,13 +93,19 @@ def haversine_distance(lat1, lat2, lon1, lon2) -> float:
 
 def calculate_total_distance(data) -> pd.DataFrame:
     modified_data = data.copy()
-    modified_data['START_POINT'] = modified_data['POLYLINE'].apply(lambda value: value[0])
-    modified_data['DEST_POINT'] = modified_data['POLYLINE'].apply(lambda value: value[-1])
-    modified_data['TOTAL_DISTANCE_KM'] = modified_data.apply(lambda row:
-                                                             haversine_distance(lat1=row.START_POINT[1],
-                                                                                lat2=row.DEST_POINT[1],
-                                                                                lon1=row.START_POINT[0],
-                                                                                lon2=row.DEST_POINT[0]),
-                                                             axis=1
-                                                             )
+    modified_data["START_POINT"] = modified_data["POLYLINE"].apply(
+        lambda value: value[0]
+    )
+    modified_data["DEST_POINT"] = modified_data["POLYLINE"].apply(
+        lambda value: value[-1]
+    )
+    modified_data["TOTAL_DISTANCE_KM"] = modified_data.apply(
+        lambda row: haversine_distance(
+            lat1=row.START_POINT[1],
+            lat2=row.DEST_POINT[1],
+            lon1=row.START_POINT[0],
+            lon2=row.DEST_POINT[0],
+        ),
+        axis=1,
+    )
     return modified_data
--- tests/unit_tests/test_distances.py
+++ tests/unit_tests/test_distances.py
@@ -4,4 +4,4 @@ from src.preprocessing import preprocessing
 def test_haversine_distance(capsys):
     distance_ = preprocessing.haversine_distance(0.4, 0.2, 0.5, 0.1)
     captured = capsys.readouterr()
-    assert(distance_ == 2.0)
+    assert distance_ == 2.0

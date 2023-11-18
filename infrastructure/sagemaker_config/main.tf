a46f10fe7a853d221c0abfdf998a25685ae80c5d
Julia Hehl <julia.hehl@bmw.de>
2023-11-18 13:38:03 +0100

    Added terraform file to upload sm lifecycle config

---
+++ infrastructure/sagemaker_config/main.tf
@@ -0,0 +1,4 @@
+resource "aws_sagemaker_notebook_instance_lifecycle_configuration" "lc" {
+  name      = "sm-notebook-autostop"
+  on_start  = base64encode("./ml-project-taxi-prediction/scripts/sagemaker_instance/sm-autostop.sh")
+}

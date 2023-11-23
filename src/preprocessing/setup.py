bd7a96bb9ddd4864797a2b914fd74219ffc4635a
Julia Hehl <julia.hehl@bmw.de>
2023-11-23 19:01:46 +0100

    Add dependency to setup file and refactor etl script

--- src/preprocessing/setup.py
+++ src/preprocessing/setup.py
@@ -9,5 +9,6 @@ setup(
     setup_requires=['wheel'],
     name="dataingestion", #Name
     version="1.0", #Version
-    packages = find_packages()  # Automatically find the packages that are recognized in the '__init__.py'.
+    packages = find_packages(),  # Automatically find the packages that are recognized in the '__init__.py'.
+    install_requires=['pandas']
 )

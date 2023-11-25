1ee05b4b825dd4317d18ea365ff9cf3f827e66bf
Julia Hehl <julia.hehl@bmw.de>
2023-11-25 13:05:39 +0100

    Adjusted code to single files

--- ml_pipeline/glue/job/preprocessing_etl.py
+++ ml_pipeline/glue/job/preprocessing_etl.py
@@ -2,6 +2,7 @@ import sys, os
 import boto3
 import time
 import pandas as pd
+import awswrangler as wr
 from string import Template
 from awsglue.transforms import *
 from awsglue.utils import getResolvedOptions
@@ -24,21 +25,25 @@ job.init(args["JOB_NAME"], args)
 
 file_path = "s3://think-tank-casestudy/raw_data/"
 env = args.get("env", "dev")
+source_bucket =  "think-tank-casestudy"
+source_prefix = "raw_data"
 target_bucket = args.get("target_bucket", f"test-database-jh") 
 prefix = args.get("prefix", "data/ingestion_data/trips")
 
 try:
-    for _,_,files in os.walk(file_path):
-        for current_file in files:
-            current_df = pd.read_parquet(current_file)
-            
-            if not current_df.empty:
+    s3_client = boto3.client("s3")
+    result = s3_client.list_objects(Bucket=source_bucket, Prefix=source_prefix)
+    file_paths_list = [f"s3://{source_bucket}/" + content_['Key'] for content_ in result['Contents'] if  content_['Key'].endswith(".parquet")]
+    for file_ in file_paths_list:   
+        current_df = wr.s3.read_parquet(path=file_)
+    
+    if not current_df.empty:
                 logger.info(f"Writing data to s3://{target_bucket}/{prefix}")
                 current_df.astype(str).to_parquet(
                     f's3://{target_bucket}/{prefix}',
                     partition_cols=["taxi_id"]
             )
-            logger.info(f"Number of rows: {current_df.shape[0]}")
+                logger.info(f"Number of rows: {current_df.shape[0]}")
 
 except Exception as e:
     logger.error("Failed when executing query with error:" , e)

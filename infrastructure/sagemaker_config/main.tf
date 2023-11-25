b1e287a6e5bb82f571c44a505587763af220a6c8
Julia Hehl <julia.hehl@bmw.de>
2023-11-25 19:50:59 +0100

    refactoring paths in sagemaker lifecycle config

--- infrastructure/sagemaker_config/main.tf
+++ infrastructure/sagemaker_config/main.tf
@@ -4,6 +4,6 @@ provider "aws" {
 }
 
 resource "aws_sagemaker_notebook_instance_lifecycle_configuration" "lc" {
-  name      = "sm-notebook-autostop"
-  on_start  = base64encode("./ml-project-taxi-prediction/scripts/sagemaker_instance/sm-autostop.sh")
+  name   = "sm-notebook-autostop"
+  on_start  = base64encode("s3://scripts-infrastructure-ml-pred-taxi/sagemaker/notebook/sm-autostop.sh")
 }
--- scripts/sagemaker_instance/sm-autostop.sh
+++ scripts/sagemaker_instance/sm-autostop.sh
@@ -5,11 +5,11 @@ set -e
 IDLE_TIME=3600
 
 echo "Fetching the autostop script"
-wget -O autostop.py ./ml-project-taxi-prediction/scripts/sagemaker_instance/autostop.py
+wget -O autostop.py s3://scripts-infrastructure-ml-pred-taxi/sagemaker/notebook/autostop.py
 
 echo "Starting the SageMaker autostop script in cron"
 (crontab -l 2>/dev/null; echo "*/5 * * * * /bin/bash -c '/usr/bin/python3 $DIR/autostop.py --time ${IDLE_TIME} | tee -a /home/ec2-user/SageMaker/auto-stop-idle.log'") | crontab -
 
 echo "Changing cloudwatch configuration"
-curl ./ml-project-taxi-prediction/scripts/sagemaker_instance/sm-autostop.sh | sudo bash -s auto-stop-idle /home/ec2-user/SageMaker/auto-stop-idle.log
+curl s3://scripts-infrastructure-ml-pred-taxi/sagemaker/notebook/on-start.sh | sudo bash -s auto-stop-idle /home/ec2-user/SageMaker/auto-stop-idle.log
 
